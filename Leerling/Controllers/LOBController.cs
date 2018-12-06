2355aa2d7317eb6f4a78c60f6d0576bbaac46394
MaurICT <31762519+maurictg@users.noreply.github.com>
2018-12-06 20:38:38 +0100

    Add files via upload
    Fixing some issues.

---
+++ Leerling/Controllers/LOBController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LIST.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LIST.Areas.Leerling.Controllers
+{
+    [Area("Leerling")]
+    [Route("leerling/[controller]")]
+    public class LOBController : Controller
+    {
+        private readonly LOBContext _context;
+
+        public LOBController(LOBContext context)
+        {
+            _context = context;
+        }
+        // GET: LOB/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Leerling/Models/ProfielModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace LIST.Areas.Leerling.Models
+{
+    public class ProfielModel
+    {
+        public bool UpdateKeuze(Profiel profiel, string token)
+        {
+            token = token.Remove(0, 1);
+            string keuze = profiel.lijn;
+
+            SqlConnection connection = new SqlConnection(LIST.Classes.Engine.connectionstring());
+            SqlCommand command = new SqlCommand("UPDATE Profielkeuze SET keuzelijn=@keuzelijn WHERE token=@token", connection);
+            command.Parameters.AddWithValue("@keuzelijn", keuze);
+            command.Parameters.AddWithValue("@token", token);
+            connection.Open();
+            try
+            {
+                command.ExecuteNonQuery();
+                connection.Close();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                LIST.Classes.Engine.WriteError(ex.ToString(), "geen");
+                connection.Close();
+                return false;
+            }
+
+        }
+
+
+
+        public Profiel profielOphalen(string token)
+        {
+            Profiel prof = new Profiel();
+
+            try
+            {
+                token = token.Remove(0, 1);
+                SqlConnection connection = new SqlConnection(LIST.Classes.Engine.connectionstring());
+                SqlCommand command = new SqlCommand("SELECT * FROM Profielkeuze WHERE token=@token", connection);
+                command.Parameters.AddWithValue("@token", token);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    if(reader["definitief"].ToString() == "1")
+                    {
+                        prof.gekozen = true;
+                    }
+                    else
+                    {
+                        prof.gekozen = false;
+                    }
+                    if(reader["keuzelijn"] == null)
+                    {
+                        prof.lijn = "Geen gekozen";
+                    }
+                    else
+                    {
+                        prof.lijn = reader["keuzelijn"].ToString();
+                    }
+                }
+                connection.Close();
+
+                return prof;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
